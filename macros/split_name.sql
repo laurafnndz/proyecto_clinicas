b402b46b35d1542f33e90f8a59d6c161cd485754
Laura Fernandez <laurafdzr@yahoo.es>
2026-05-07 17:34:20 +0000

    07-05-2026

--- macros/split_name.sql
+++ macros/split_name.sql
@@ -1,7 +1,9 @@
-#Macro para separar en diferentes columnas el nombre, apellido, apellido que vienen de origen en una única columna
+--Macro para separar en diferentes columnas el nombre, apellido, apellido que vienen de origen en una única columna
 
 {% macro split_name(column) %}
     SPLIT_PART({{ column }}, ' ', 1) AS nombre,
     SPLIT_PART({{ column }}, ' ', 2) AS primer_apellido,
     SPLIT_PART({{ column }}, ' ', 3) AS segundo_apellido
 {% endmacro %}
+
+-- la llamamos con esto {{ split_name('nombre_dueno') }},   --
