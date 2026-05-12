2391961c1f306794e7aa5f47c654389564454ad2
Laura Fernandez <laurafernandezrodriguez.info@gmail.com>
2026-05-12 21:31:08 +0000

    creacion capa gold (necesita cambios)

---
+++ models/marts/dim_empleado.sql
@@ -0,0 +1,22 @@
+-- marts/dim_empleado.sql
+{{ config(materialized='table') }}
+
+select
+    e.dbt_scd_id              as id_version,
+    e.id_empleado,
+    e.nombre,
+    e.primer_apellido,
+    e.segundo_apellido,
+    e.dni,
+    e.numero_colegiado,
+    e.salario,
+    p.puesto,
+    e.id_centro,
+    e.dbt_valid_from,
+    e.dbt_valid_to,
+    case when e.dbt_valid_to is null 
+         then true else false end    as es_actual --cuando valid_to not null: actual es true
+                                                    --cuando es null: actual es false
+from {{ ref('snp_empleado') }} e
+left join {{ ref('stg__puesto') }} p
+    on e.id_puesto = p.id_puesto
